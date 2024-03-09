0b7657dbf7c401666193405163c2d0894c08a6f5
ZeroYangxi <riveryangxi@gmail.com>
2024-03-09 18:29:40 -0500

    create data.json

--- map.js
+++ map.js
@@ -105,22 +105,99 @@ svg
     d3.select(this).style("stroke", "none").style("stroke-width", 0);
   })
   .on("click", function (event, d) {
-    drawBarChart(d);
+    updateDensityPlot(d.group); // Assuming d.group identifies the dataset or parameter for the density plot
   }); // Function to draw the bar chart;
 
-function drawBarChart(data) {
-  console.log("clicked");
-  // Clear the existing chart
-  d3.select("#barChartContainer").html("");
+function updateDensityPlot(group) {
+  // Fetch or select data related to 'group'
+  //  let dataDensity = ; // This depends on your data structure
 
-  // Set up a new SVG container or use an existing one
-  const svgBarChart = d3
-    .select("#barChartContainer")
+  // Clear previous density plot
+  d3.select("#densityPlot").select("svg").remove();
+
+  // Draw new density plot based on 'data'
+  drawDensityPlot(data);
+}
+
+function drawDensityPlot(data) {
+  // Define dimensions and margins for the density plot
+  let margin = { top: 30, right: 30, bottom: 30, left: 50 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+  // Clear previous plots
+  d3.select("#densityPlot").select("svg").remove();
+
+  // Append the svg object to the density plot container
+  let svg = d3
+    .select("#densityPlot")
     .append("svg")
-    .attr("width", 400)
-    .attr("height", 200)
-    .append("g");
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // Your bar chart drawing logic here
-  // Use 'data' to determine what to display
+  // Set up X axis
+  let x = d3
+    .scaleLinear()
+    .domain([0, 1000]) // This domain might need to be dynamic based on `data`
+    .range([0, width]);
+  let
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Set up Y axis
+  let y = d3.scaleLinear().range([height, 0]).domain([0, 0.01]); // This domain might also need to be dynamic
+  svg.append("g").call(d3.axisLeft(y));
+
+  // Compute kernel density estimation (KDE)
+  let kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40));
+  let density = kde(
+    data.map(function (d) {
+      return d.value;
+    })
+  ); // Assuming `data` is an array of objects with a `value` property
+
+  // Plot the KDE as a path
+  svg
+    .append("path")
+    .datum(density)
+    .attr("fill", "#69b3a2")
+    .attr("opacity", ".8")
+    .attr("stroke", "#000")
+    .attr("stroke-width", 1)
+    .attr("stroke-linejoin", "round")
+    .attr(
+      "d",
+      d3
+        .line()
+        .curve(d3.curveBasis)
+        .x(function (d) {
+          return x(d[0]);
+        })
+        .y(function (d) {
+          return y(d[1]);
+        })
+    );
+}
+
+// Kernel Density Estimator function
+function kernelDensityEstimator(kernel, X) {
+  return function (V) {
+    return X.map(function (x) {
+      return [
+        x,
+        d3.mean(V, function (v) {
+          return kernel(x - v);
+        }),
+      ];
+    });
+  };
+}
+
+function kernelEpanechnikov(k) {
+  return function (v) {
+    return Math.abs((v /= k)) <= 1 ? (0.75 * (1 - v * v)) / k : 0;
+  };
 }
